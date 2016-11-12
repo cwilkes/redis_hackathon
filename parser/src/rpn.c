8f17decacd680fedfa4afd12bfd1b8b054cfd2ff
Chris Wilkes <cwilkes@gmail.com>
2016-11-12 09:59:00 -0800

    working parser

--- parser/src/rpn.c
+++ parser/src/rpn.c
@@ -5,24 +5,7 @@
 
 // taken from http://codereview.stackexchange.com/questions/79738/rpn-calculator-in-c
 
-/* RPN: solve RPN posted */
-int RpnSolve(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-  if (argc < 2) return RedisModule_WrongArity(ctx);
-  return RedisModule_ReplyWithString(ctx, argv[1]);
-}
-
-/* Registering the module */
-int RedisModule_OnLoad(RedisModuleCtx *ctx) {
-  if (RedisModule_Init(ctx, "rpn", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
-    return REDISMODULE_ERR;
-  }
-  if (RedisModule_CreateCommand(ctx, "rpn.solve", RpnSolve, "readonly", 1,1,1) == REDISMODULE_ERR) {
-    return REDISMODULE_ERR;
-  }
-	return 0;
-}
-
-void push(float stack[], float value, int *currStack)
+void push(double stack[], double value, int *currStack)
 {
     int i = *currStack;
 
@@ -36,7 +19,7 @@ void push(float stack[], float value, int *currStack)
     *currStack += 1;
 }
 
-void pop(float stack[], char operation, int *currStack)
+void pop(double stack[], char operation, int *currStack)
 {
     int i;
 
@@ -58,7 +41,7 @@ void pop(float stack[], char operation, int *currStack)
             printf("Invalid character.");
     }
 
-    float temp = stack[1];
+    double temp = stack[1];
     for (i=1; i < *currStack; i++) {
         stack[i] = stack[i+1];
     }
@@ -79,20 +62,41 @@ int is_operator(char c) {
     }
 }
 
-void decode(char **instring, float *outval, int size)
+void decode(RedisModuleCtx *ctx, RedisModuleString **instring, double *outval, int size)
 {
     int i=0, currStack=0;
-    float stack[size/2];
-    float temp;
+    double stack[size/2];
+    double temp;
+		size_t len;
     for (i=1;i<size;i++)
-    {        
-				if ( ! instring[i][1] && is_operator(instring[i][0] )) {
-						pop(stack, *instring[i], &currStack);
-				} else if ( (temp = atof(instring[i])) || ! strcmp(instring[i], "0.0") ) {
+    {       
+        const char *val = RedisModule_StringPtrLen(instring[i], &len);
+				if ( ! val[1] && is_operator(val[0] )) {
+						pop(stack, *val, &currStack);
+				} else if ( (temp = atof(val)) || ! strcmp(val, "0.0") ) {
 						push(stack, temp, &currStack);
 				} else {
-						printf("Invalid argument:  [%s].", instring[i]);
+						RedisModule_Log(ctx, "warning", "Invalid argument:  [%s].", val);
 				}   
     }
     *outval = stack[0];
+}
+
+/* RPN: solve RPN posted */
+double RpnSolve(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+	double result;
+	size_t len;
+	decode(ctx, argv, &result, argc);
+  return RedisModule_ReplyWithDouble(ctx, result);
+}
+
+/* Registering the module */
+int RedisModule_OnLoad(RedisModuleCtx *ctx) {
+  if (RedisModule_Init(ctx, "rpn", 3, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+  if (RedisModule_CreateCommand(ctx, "rpn.solve", RpnSolve, "readonly", 1,1,1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+	return 0;
 }
